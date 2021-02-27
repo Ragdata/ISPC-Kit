4095b0679e76f69e1b2454363cb7d18d072827f8
Ragdata <ragdata@users.noreply.github.com>
2021-02-27 10:42:03 +1000

    feat(swap): make swap launchable

---
+++ src/lib/swap.sh
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------
+# src/lib/swap.sh
+#-------------------------------------------------------------------
+# ISPC Kit - ISPConfig 3 Installer
+#
+# File:         swap.sh
+# Author:       Ragdata
+# Date:         26/01/2021 0035
+# License:      MIT License
+# Copyright:    2021 ~ Aequitas Veritas Pty Ltd ~ All Rights Reserved
+#-------------------------------------------------------------------
+# MAIN
+#-------------------------------------------------------------------
+clear
+echoLog "Allocate Swap Space"
+echoLog "line"
+echoLog "spacer"
+
+checkSWAP
