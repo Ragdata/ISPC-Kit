6cca7136831706f43a64a0f33396ace02e4e49c5
Ragdata <6487363-Ragdata@users.noreply.gitlab.com>
2021-03-02 08:12:50 +1000

    fix(issue#2): fix unit tests

--- test/scripts/test_libs.sh
+++ test/scripts/test_libs.sh
@@ -12,16 +12,16 @@
 #-------------------------------------------------------------------
 # INITIALISE
 #-------------------------------------------------------------------
-baseDir=$(dirname "${BASH_SOURCE[0]}")
+baseDir="./../.."
 #-------------------------------------------------------------------
 # LOAD LIBRARIES
 #-------------------------------------------------------------------
-. "$baseDir/src/lib/common.sh" || return 1
-. "$formats" || return 1
-. "$functions" || return 1
-. "$logger" || return 1
-. "$register" || return 1
-. "$yaml" || return 1
+. "$baseDir/src/lib/common.sh" || exit 1
+. "$formats" || exit 1
+. "$functions" || exit 1
+. "$logger" || exit 1
+. "$register" || exit 1
+. "$yaml" || exit 1
 # and it really is that simple - as long as the libraries are loading with
 # no problems, that's all I need this test to do!
-return 0
+exit 0
