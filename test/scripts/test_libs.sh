e4117d794b5e3acda8eef11a1ce23d71908a4144
Ragdata <6487363-Ragdata@users.noreply.gitlab.com>
2021-03-02 07:41:35 +1000

    feat(apps): add placeholders for documize & go

---
+++ test/scripts/test_libs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#-------------------------------------------------------------------
+# test/scripts/test_libs.sh
+#-------------------------------------------------------------------
+# ISPC Kit - ISPConfig 3 Installer
+#
+# File:         test_libs.sh
+# Author:       Ragdata
+# Date:         02/03/2021 0348
+# License:      MIT License
+# Copyright:    2021 ~ Aequitas Veritas Pty Ltd ~ All Rights Reserved
+#-------------------------------------------------------------------
+# INITIALISE
+#-------------------------------------------------------------------
+baseDir=$(dirname "${BASH_SOURCE[0]}")
+#-------------------------------------------------------------------
+# LOAD LIBRARIES
+#-------------------------------------------------------------------
+. "$baseDir/src/lib/common.sh" || exit 1
+. "$formats" || exit 1
+. "$functions" || exit 1
+. "$logger" || exit 1
+. "$register" || exit 1
+. "$yaml" || exit 1
+# and it really is that simple - as long as the libraries are loading with
+# no problems, that's all I need this test to do!
+exit 0
---
+++ test/scripts/test_registry.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#-------------------------------------------------------------------
+# test/scripts/test_libs.sh
+#-------------------------------------------------------------------
+# ISPC Kit - ISPConfig 3 Installer
+#
+# File:         test_libs.sh
+# Author:       Ragdata
+# Date:         02/03/2021 0348
+# License:      MIT License
+# Copyright:    2021 ~ Aequitas Veritas Pty Ltd ~ All Rights Reserved
+#-------------------------------------------------------------------
+# INITIALISE
+#-------------------------------------------------------------------
+baseDir=$(dirname "${BASH_SOURCE[0]}")
+#-------------------------------------------------------------------
+# LOAD LIBRARIES
+#-------------------------------------------------------------------
+. "$baseDir/src/lib/common.sh" || exit 1
+. "$formats" || exit 1
+. "$functions" || exit 1
+. "$logger" || exit 1
+. "$register" || exit 1
+. "$yaml" || exit 1
+#-------------------------------------------------------------------
+# MAIN TEST
+#-------------------------------------------------------------------
+# initialise registry
+declare -A REGISTRY
+# assign a few variables
+REGISTRY[LOC]="en_AU.UTF-8"
+REGISTRY[EMAIL]="admin@aever.net"
+REGISTRY[IP]="192.168.0.1"
+# serialize the registry
+serialize REGISTRY || exit 1
+# truncate the registry(?)
+declare -A REGISTRY || exit 1
+# print the file just for shits'n'giggles
+if [[ ! -f "$registry/.registry" ]]; then exit 1;
+else cat "$registry/.registry"; fi
+# unserialize the registry
+unserialize "$registry/.registry" || exit 1
+# check that an expected variable exists
+if [[ ${REGISTRY[LOC]} != "en_AU.UTF-8" ]]; then exit 1; fi
+# SUCCESS!
+exit 0;
