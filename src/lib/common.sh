c6c6e42ed3e44197103953b73dcb762ed082874c
Ragdata <6487363-Ragdata@users.noreply.gitlab.com>
2021-03-01 16:05:56 +1000

    refactor(project): pre-release candidate v-0.1.0

--- src/lib/common.sh
+++ src/lib/common.sh
@@ -13,56 +13,56 @@
 # COMMON ELEMENTS
 #-------------------------------------------------------------------
 # regular expressions
-export HOST_REGEX='^[a-z0-9][a-z0-9\-]+\.[a-z0-9\.\-]+$'
-export IPv4_REGEX='^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]?[0-9])\.?){4}$'
-export IPv6_REGEX='^([a-fA-F0-9]{0,4}:?){6}([a-fA-F0-9]{1,4})$'
-export isFlag="^\-.*"
-export isLoc="^[a-z]{2}_[A-Z]{2}.*$"
-export isPath="^(/[^/]*)*(/?.+(\..+))$"
-export AFFIRM='^[Yy]$'
-export NEGAT='^[Nn]$'
-export RESP='^[YyNn]$'
+HOST_REGEX='^[a-z0-9][a-z0-9\-]+\.[a-z0-9\.\-]+$'
+IPv4_REGEX='^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]?[0-9])\.?){4}$'
+IPv6_REGEX='^([a-fA-F0-9]{0,4}:?){6}([a-fA-F0-9]{1,4})$'
+isFlag="^\-.*"
+isLoc="^[a-z]{2}_[A-Z]{2}.*$"
+isPath="^(/[^/]*)*(/?.+(\..+))$"
+AFFIRM='^[Yy]$'
+NEGAT='^[Nn]$'
+RESP='^[YyNn]$'
 # printable elements
-export DEFAULT_Y="[${bold}Y${NC}/N]"
-export DEFAULT_N="[Y/${bold}N${NC}]"
+DEFAULT_Y="[${bold}Y${NC}/N]"
+DEFAULT_N="[Y/${bold}N${NC}]"
 #-------------------------------------------------------------------
 # PATHS
 #-------------------------------------------------------------------
-export rootDir=/root/.ispc
-export logDir="$rootDir/logs"
+rootDir=/root/.ispc
+logDir="$rootDir/logs"
 if [[ ! -d $logDir ]]; then mkdir "$logDir"; fi
 
-export registry="$baseDir/registry"
+registry="$baseDir/registry"
 if [[ ! -h $registry ]]; then ln -s "$registry" "$rootDir"; fi
 
-export cfgDir="$baseDir/cfg"
-export nginxDir="$cfgDir/nginx"
+cfgDir="$baseDir/cfg"
+nginxDir="$cfgDir/nginx"
 
-export docsDir="$baseDir/docs"
+docsDir="$baseDir/docs"
 
-export srcDir="$baseDir/src"
-export scriptsDir="$srcDir/scripts"
-export appsDir="$scriptsDir/apps"
-export elemDir="$scriptsDir/elem"
+srcDir="$baseDir/src"
+scriptsDir="$srcDir/scripts"
+appsDir="$scriptsDir/apps"
+elemDir="$scriptsDir/elem"
 
-export sqlDir="$baseDir/sql"
+sqlDir="$baseDir/sql"
 if [[ ! -d $sqlDir ]]; then mkdir "$sqlDir"; fi
 #-------------------------------------------------------------------
 # CODE FILES
 #-------------------------------------------------------------------
-#export crypter="$etcDir"/crypter.sh
-#export errors="$etcDir"/error.sh
-export yaml="$etcDir/yaml.sh"
-export common="$libDir/common.sh"
-export formats="$libDir/format.sh"
-export functions="$libDir/functions.sh"
-export register="$libDir/registry.sh"
+#crypter="$etcDir"/crypter.sh
+#errors="$etcDir"/error.sh
+yaml="$etcDir/yaml.sh"
+common="$libDir/common.sh"
+formats="$libDir/format.sh"
+functions="$libDir/functions.sh"
+register="$libDir/registry.sh"
 #-------------------------------------------------------------------
 # DATA FILES
 #-------------------------------------------------------------------
-export DEF="$cfgDir/.defaults.yml"
-export PRM="$cfgDir/.permissions.yml"
-export RMT="$cfgDir/.remote.yml"
-export REG="$registry/.registry"
-export PAS="$registry/.passwords"
-export SQL="$sqlDir/database.sql"
+DEF="$cfgDir/.defaults.yml"
+PRM="$cfgDir/.permissions.yml"
+RMT="$cfgDir/.remote.yml"
+REG="$registry/.registry"
+PAS="$registry/.passwords"
+SQL="$sqlDir/database.sql"
