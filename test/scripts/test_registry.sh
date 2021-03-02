c2d46ea6bf16e952649fa1dde9d7e7f110d1f399
Ragdata <6487363-Ragdata@users.noreply.gitlab.com>
2021-03-02 11:36:17 +1000

    fix(tests): fix registry test

--- test/scripts/test_registry.sh
+++ test/scripts/test_registry.sh
@@ -37,9 +37,12 @@ REGISTRY[IP]="192.168.0.1"
 
 echo
 # shellcheck disable=SC1087
-array="${arrayName}[@]"
-echo "$array"
-echo "${!array}"
+array="${arrayName}[*]"
+array2="${arrayName}[*]"
+array3="${arrayName}[*]"
+echo "$array || ${!array}"
+echo "$array2 || ${!array}2"
+echo "$array3 || ${!array}3"
 echo
 
 serialize REGISTRY || exit 1
