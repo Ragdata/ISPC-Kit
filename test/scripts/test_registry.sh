9bb5b5d3ccca82694a8ae518a172500796a9b468
Ragdata <6487363-Ragdata@users.noreply.gitlab.com>
2021-03-02 10:18:46 +1000

    tests(issue #2): add registry test

--- test/scripts/test_registry.sh
+++ test/scripts/test_registry.sh
@@ -33,8 +33,8 @@ REGISTRY[EMAIL]="admin@aever.net"
 REGISTRY[IP]="192.168.0.1"
 # serialize the registry
 serialize REGISTRY || exit 1
-# truncate the registry(?)
-declare -A REGISTRY || exit 1
+# unset the registry(?)
+unset REGISTRY || exit 1
 # print the file just for shits'n'giggles
 if [[ ! -f "$registry/.registry" ]]; then exit 1;
 else cat "$registry/.registry"; fi
